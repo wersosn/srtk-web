855f083a58755a5a96e44998379ab9baafca7570
WeronikaSosnowska <wersosnwsk@gmail.com>
2025-07-05 14:36:35 +0200

    Init backend+fronted

---
+++ frontend/srtk-frontend/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'https://localhost:5001', // Tu będzie Backend ASP.NET Core Web API
+    },
+  },
+});
